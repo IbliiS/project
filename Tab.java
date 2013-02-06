01038b8aad706b0236c0fa220b6e2592277aebea
IbliiS <iblis_@hotmail.fr>
2013-02-06 11:59:08 +0100

    fait : 	verifier si on doit agrandir le terrain 	agrandissement du terrain 	verrifier si on une cellule peut naitre 	verrifier si une cellule peut continuer a vivre

--- Tab.java
+++ Tab.java
@@ -11,11 +11,12 @@ public class Tab{
 		}
 	}
 	
+	//a modifier pour centrer le tableau
 	public void agrandir(){
 		int tab2[][]=new int [this.tab.length+20][this.tab.length+20];
 		for(int i=0;i<this.tab.length;i++){
 			for(int j=0;j<this.tab.length;j++){
-				tab2[i][j]=tab[i][j];
+				tab2[i+10][j+10]=tab[i][j];
 			}
 		}
 		tab=tab2;
@@ -138,4 +139,141 @@ public class Tab{
 		return (cpt==3);
 	}
 	
+	//savoir si une cellule vivante peut continuer a vivre
+	public bool verifVoisinVivante(int i,int j){
+		int cpt=0;
+		if(i==0){
+			if(j==0){
+				if(tab[i][j+1]==1)
+					cpt++;
+				if(tab[i+1][j]==1)
+					cpt++;
+				if(tab[i+1][j+1]==1)
+					cpt++;
+			}
+			else if(j==tab.length-1){
+				if(tab[i][j-1]==1)
+					cpt++;
+				if(tab[i+1][j]==1)
+					cpt++;
+				if(tab[i+1][j-1]==1)
+					cpt++;
+			}
+			else{
+				//i==0 et j qlcq
+				if(tab[i][j-1]==1)
+					cpt++;
+				if(tab[i][j+1]==1)
+					cpt++;
+				if(tab[i+1][j]==1)
+					cpt++;
+				if(tab[i+1][j+1]==1)
+					cpt++;
+				if(tab[i+1][j-1]==1)
+					cpt++;
+			}
+			
+		}
+		else if(i==tab.length-1){
+			if(j==0){
+				if(tab[i-1][j]==1)
+					cpt++;
+				if(tab[i-1][j+1]==1)
+					cpt++;
+				if(tab[i][j+1]==1)
+					cpt++;
+			}
+			else if(j==tab.length-1){
+				if(tab[i-1][j]==1)
+					cpt++;
+				if(tab[i-1][j-1]==1)
+					cpt++;
+				if(tab[i][j-1]==1)
+					cpt++;
+			}
+			else{
+				//i==tab.length-1 et j qlcq
+				if(tab[i-1][j]==1)
+					cpt++;
+				if(tab[i-1][j-1]==1)
+					cpt++;
+				if(tab[i-1][j+1]==1)
+					cpt++;
+				if(tab[i][j-1]==1)
+					cpt++;
+				if(tab[i][j+1]==1)
+					cpt++;
+			}
+		}
+		else{
+			if(j==0){
+				//i qlcq et j==0
+				if(tab[i-1][j]==1)
+					cpt++;
+				if(tab[i-1][j+1]==1)
+					cpt++;
+				if(tab[i][j+1]==1)
+					cpt++;
+				if(tab[i+1][j]==1)
+					cpt++;
+				if(tab[i+1][j+1]==1)
+					cpt++;
+				
+			}
+			else if(j==tab.length-1){
+				//i qlcq et j==tab.length-1
+				if(tab[i-1][j]==1)
+					cpt++;
+				if(tab[i-1][j-1]==1)
+					cpt++;
+				if(tab[i][j-1]==1)
+					cpt++;
+				if(tab[i+1][j]==1)
+					cpt++;
+				if(tab[i+1][j-1]==1)
+					cpt++;
+			}
+			else{
+				//i qlqc et j qlcq
+				if(tab[i-1][j]==1)
+					cpt++;
+				if(tab[i-1][j-1]==1)
+					cpt++;
+				if(tab[i-1][j+1]==1)
+					cpt++;
+				if(tab[i][j-1]==1)
+					cpt++;
+				if(tab[i][j+1]==1)
+					cpt++;
+				if(tab[i+1][j]==1)
+					cpt++;
+				if(tab[i+1][j+1]==1)
+					cpt++;
+				if(tab[i+1][j-1]==1)
+					cpt++;
+			}	
+		}
+		return (cpt>=2);
+	}
+	
+	//savoir si on doit agrandir ? oui si il y a + de 2 C sur un des cotés
+	public bool doitOnAgrandir(){
+		int i=0;
+		int j=0;
+		int cpt=0;
+		for(j=0;j<tab.length;j++){
+			if(tab[i][j]==1)
+				cpt++;
+			if(tab[j][i]==1)
+				cpt++;
+		}
+		i=tab.length-1;
+		for(j=0;j<tab.length;j++){
+			if(tab[i][j]==1)
+				cpt++;
+			if(tab[j][i]==1)
+				cpt++;
+		}
+		return (cpt>2);
+	}
 }
